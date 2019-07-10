c7e99178feb2b46dacb5069476ccf23fe5c17538
YA <YA-androidapp@users.noreply.github.com>
2019-07-10 12:36:08 +0900

    &copy;

--- 02_index_face.py
+++ 02_index_face.py
@@ -1,4 +1,9 @@
-﻿import boto3
+﻿#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
+import boto3
 import glob
 import json
 import os
